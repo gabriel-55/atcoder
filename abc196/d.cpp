f10989e1b82168e29b4c29c30e6c116eb9bc19dd
Gabriel <renokoku@gmail.com>
2022-08-01 23:33:15 +1200

    commit

---
+++ abc196/d.cpp
@@ -0,0 +1,89 @@
+#if !__INCLUDE_LEVEL__
+#include __FILE__
+
+int main(void){
+   ll h, w, a, b;
+   cin >> h >> w >> a >> b;
+   
+   ll ans = 0;
+   vector used(h, vector<bool>(w, false));
+   auto dfs = [&](auto dfs, ll x, ll y) -> void {
+      if(b < 0 || a < 0) return;
+      if(y == w) {
+         x++;
+         y = 0;
+      }
+      if(x == h) {
+         ans++;
+         return;
+      }
+      
+      if(used[x][y]) {
+         dfs(dfs, x, y+1);
+         return;
+      }
+
+      used[x][y] = true;;
+      b--;
+      dfs(dfs, x, y+1);
+      b++;
+      
+      a--;
+      if(x+1 < h && !used[x+1][y]) {
+         used[x+1][y] = true;
+         dfs(dfs, x, y+1);
+         used[x+1][y] = false;
+      }
+      if(y+1 < w && !used[x][y+1]) {
+         used[x][y+1] = true;
+         dfs(dfs, x, y+1);
+         used[x][y+1] = false;
+      }
+      a++;
+      used[x][y] = false;
+      return;
+   };
+
+   dfs(dfs, 0, 0);
+   cout << ans << '\n';
+   return 0;
+}
+
+/*---------------------------------------------------------------------------------------------------
+　　 　　 ○＿＿＿＿＿_
+　  　 　 ||　 　 　 |
+　 　  　 ||　  ● 　 |
+　 　  　 ||　 　 　 |
+ 　   　  ||￣￣￣￣￣
+　　   　 ||  君が代は
+   ∧__,,∧||　 千代に八千代に
+　 ( ｀･ω･||    さざれ石の巌となりて
+　  ヽ  つ０     こけのむすまで
+　　 し―-J
+---------------------------------------------------------------------------------------------------*/
+
+#else
+#include <bits/stdc++.h>
+using namespace std;
+#include <atcoder/all>
+using namespace atcoder;
+using ll = long long;
+using ull = unsigned long long;
+using ldb = long double;
+using P = pair<ll, ll>;
+#define fi first
+#define se second
+#define REP(i,a,b) for (ll i = a, i##_range = (b); i < i##_range; i++)
+#define rep(i,a) REP(i,0,a)
+template <class T> bool chmin(T& a, const T& b) { return a > b ? a = b, true : false; }
+template <class T> bool chmax(T& a, const T& b) { return a < b ? a = b, true : false; }
+#ifdef _DEBUG
+#include <debug.hpp>
+#define debug(...) debug::dprint(#__VA_ARGS__, __VA_ARGS__)
+#else
+#define debug(...) (static_cast<void>(0))
+#endif
+struct initialise { initialise() { cin.tie(nullptr); ios::sync_with_stdio(false); cout << fixed << setprecision(20); }; }__INI__;
+const ll inf = 1LL << 60;
+const ll dx[4] = {-1, 1, 0, 0}, dy[4] = {0, 0, -1, 1};
+#endif
