2b35671ed3f33d826ee0e882e99c02488eaa9237
Gabriel <renokoku@gmail.com>
2022-08-05 19:43:30 +1200

    commit

---
+++ abc040/c.cpp
@@ -0,0 +1,58 @@
+#if !__INCLUDE_LEVEL__
+#include __FILE__
+
+int main(void){
+   ll n;
+   cin >> n;
+   
+   vector<ll> a(n);
+   rep(i,n) cin >> a[i];
+   vector<ll> dp(n, 0);
+
+   REP(i,1,n) {
+      dp[i] = dp[i-1] + abs(a[i] - a[i-1]);
+      if(i-2 >= 0) dp[i] = min(dp[i], dp[i-2]+abs(a[i] - a[i-2]));
+   }
+
+   cout << dp[n-1]  << '\n';
+   return 0; 
+}
+
+/*---------------------------------------------------------------------------------------------------
+　　 　　 ○＿＿＿＿＿_
+　  　 　 ||　 　 　 |
+　 　  　 ||　  ● 　 |
+　 　  　 ||　 　 　 |
+ 　   　  ||￣￣￣￣￣
+　　   　 ||  君が代は
+   ∧__,,∧||　 千代に八千代に
+　 ( ｀･ω･||    さざれ石の巌となりて
+　  ヽ  つ０     こけのむすまで
+　　 し―-J
+---------------------------------------------------------------------------------------------------*/
+
+#else
+#include <bits/stdc++.h>
+using namespace std;
+#include <atcoder/all>
+using namespace atcoder;
+using ll = long long;
+using ull = unsigned long long;
+using ldb = long double;
+using P = pair<ll, ll>;
+#define fi first
+#define se second
+#define REP(i,a,b) for (ll i = a, i##_range = (b); i < i##_range; i++)
+#define rep(i,a) REP(i,0,a)
+template <class T> bool chmin(T& a, const T& b) { return a > b ? a = b, true : false; }
+template <class T> bool chmax(T& a, const T& b) { return a < b ? a = b, true : false; }
+#ifdef _DEBUG
+#include <debug.hpp>
+#define debug(...) debug::dprint(#__VA_ARGS__, __VA_ARGS__)
+#else
+#define debug(...) (static_cast<void>(0))
+#endif
+struct initialise { initialise() { cin.tie(nullptr); ios::sync_with_stdio(false); cout << fixed << setprecision(20); }; }__INI__;
+const ll inf = 1LL << 60;
+const ll dx[4] = {-1, 1, 0, 0}, dy[4] = {0, 0, -1, 1};
+#endif
