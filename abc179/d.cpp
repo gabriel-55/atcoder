d27b2fadf9129275158730f3b91994a635ae6a98
Gabriel <renokoku@gmail.com>
2022-08-30 00:26:03 +1200

    commit

---
+++ abc179/d.cpp
@@ -0,0 +1,67 @@
+#if !__INCLUDE_LEVEL__
+#include __FILE__
+
+const ll mod = 998244353;
+
+int main(void){
+   ll n, k;
+   cin >> n >> k;
+
+   vector<ll> l(k), r(k);
+   rep(i,k) cin >> l[i] >> r[i];
+
+   vector<ll> dp(n), a(n);
+   dp[0] = 1;
+   ll ch = 0;
+   rep(i,n) {
+      ch += a[i];
+      dp[i] += ch;
+      dp[i] %= mod;
+      rep(j,k) { 
+         if(i+l[j] < n) a[i+l[j]] += dp[i];
+         if(i+r[j]+1 < n) a[i+r[j]+1] -= dp[i];
+      }
+   }
+
+   cout << dp[n-1] << '\n';
+   return 0;
+}
+
+/*---------------------------------------------------------------------------------------------------
+　　 　　 ○＿＿＿＿＿_
+　  　 　 ||　 　 　 |
+　 　  　 ||　  ● 　 |
+　 　  　 ||　 　 　 |
+ 　   　  ||￣￣￣￣￣
+　　   　 ||  君が代は
+   ∧__,,∧||　 千代に八千代に
+　 ( ｀･ω･||    さざれ石の巌となりて
+　  ヽ  つ０     こけのむすまで
+　　 し―-J
+---------------------------------------------------------------------------------------------------*/
+
+#else
+#include <bits/stdc++.h>
+using namespace std;
+#include <atcoder/all>
+using namespace atcoder;
+using ll = long long;
+using ull = unsigned long long;
+using ldb = long double;
+using P = pair<ll, ll>;
+#define fi first
+#define se second
+#define REP(i,a,b) for (ll i = a, i##_range = (b); i < i##_range; i++)
+#define rep(i,a) REP(i,0,a)
+template <class T> bool chmin(T& a, const T& b) { return a > b ? a = b, true : false; }
+template <class T> bool chmax(T& a, const T& b) { return a < b ? a = b, true : false; }
+#ifdef _DEBUG
+#include <debug.hpp>
+#define debug(...) debug::dprint(#__VA_ARGS__, __VA_ARGS__)
+#else
+#define debug(...) (static_cast<void>(0))
+#endif
+struct initialise { initialise() { cin.tie(nullptr); ios::sync_with_stdio(false); cout << fixed << setprecision(20); }; }__INI__;
+const ll inf = 1LL << 60;
+const ll dx[4] = {-1, 1, 0, 0}, dy[4] = {0, 0, -1, 1};
+#endif
