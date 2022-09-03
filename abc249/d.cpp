7bf1ec945240eddda7e26244b7604db52de08322
Gabriel <renokoku@gmail.com>
2022-09-03 18:40:00 +1200

    commit

---
+++ abc249/d.cpp
@@ -0,0 +1,63 @@
+#if !__INCLUDE_LEVEL__
+#include __FILE__
+
+int main(void){
+   int n;
+   cin >> n;
+   vector<ll> v(2e5+1, 0);
+   rep(i,n){
+      int a;
+      cin >> a;
+      v[a]++;
+   }
+
+   ll ans = 0;
+   vector<ll> a(2e5+1, 0);
+   REP(i,1,2e5+1) REP(j,i,2e5+1) {
+      if(i*j > 2e5) break;
+      if(i == j) ans += v[i]*v[j]*v[i*j];
+      else ans += v[i]*v[j]*v[i*j]*2;
+   }
+
+   cout << ans << '\n';
+   return 0;
+}
+
+/*---------------------------------------------------------------------------------------------------
+　　 　　 ○＿＿＿＿＿_
+　  　 　 ||　 　 　 |
+　 　  　 ||　  ● 　 |
+　 　  　 ||　 　 　 |
+ 　   　  ||￣￣￣￣￣
+　　   　 ||  君が代は
+   ∧__,,∧||　 千代に八千代に
+　 ( ｀･ω･||    さざれ石の巌となりて
+　  ヽ  つ０     こけのむすまで
+　　 し―-J
+---------------------------------------------------------------------------------------------------*/
+
+#else
+#include <bits/stdc++.h>
+using namespace std;
+#include <atcoder/all>
+using namespace atcoder;
+using ll = long long;
+using ull = unsigned long long;
+using ldb = long double;
+using P = pair<ll, ll>;
+#define fi first
+#define se second
+#define REP(i,a,b) for (ll i = a, i##_range = (b); i < i##_range; i++)
+#define rep(i,a) REP(i,0,a)
+template <class T> bool chmin(T& a, const T& b) { return a > b ? a = b, true : false; }
+template <class T> bool chmax(T& a, const T& b) { return a < b ? a = b, true : false; }
+#ifdef _DEBUG
+#include <debug.hpp>
+#define debug(...) debug::dprint(#__VA_ARGS__, __VA_ARGS__)
+#else
+#define debug(...) (static_cast<void>(0))
+#endif
+struct initialise { initialise() { cin.tie(nullptr); ios::sync_with_stdio(false); cout << fixed << setprecision(20); }; }__INI__;
+const ll inf = 1LL << 60;
+const ll dx[4] = {-1, 1, 0, 0}, dy[4] = {0, 0, -1, 1};
+#endif
---
+++ abc250/d.cpp
@@ -0,0 +1,96 @@
+#if !__INCLUDE_LEVEL__
+#include __FILE__
+
+// エラトステネスの篩
+vector<bool> Eratosthenes(int N) {
+    // テーブル
+    vector<bool> isprime(N+1, true);
+
+    // 0, 1 は予めふるい落としておく
+    isprime[0] = isprime[1] = false;
+
+    // ふるい
+    for (int p = 2; p <= N; ++p) {
+        // すでに合成数であるものはスキップする
+        if (!isprime[p]) continue;
+
+        // p 以外の p の倍数から素数ラベルを剥奪
+        for (int q = p * 2; q <= N; q += p) {
+            isprime[q] = false;
+        }
+    }
+
+    // 1 以上 N 以下の整数が素数かどうか
+    return isprime;
+}
+
+int main(void){
+   ll n;
+   cin >> n;
+
+   vector<bool> isprime = Eratosthenes(1e6+1);
+   vector<ll> v;
+   rep(i,1e6+1) {
+      if(isprime[i]) v.emplace_back(i);
+   }
+
+   auto func = [&](ll p, ll q) -> bool {
+      double np = p, nq = q;
+      if(np*nq*nq*nq > 4e18) return false;
+      if(p*q*q*q > n) return false;
+      return true;
+   };
+
+   ll ans = 0;
+   REP(i,1,v.size()) {
+      ll ok = -1, ng = i;
+      while(abs(ok - ng) > 1) {
+         ll m = (ok + ng) / 2;
+         if(func(v[m], v[i])) ok = m;
+         else ng = m;
+     }
+     ans += ok+1;
+   }
+
+   cout << ans << '\n';
+   return 0;
+}
+
+/*---------------------------------------------------------------------------------------------------
+　　 　　 ○＿＿＿＿＿_
+　  　 　 ||　 　 　 |
+　 　  　 ||　  ● 　 |
+　 　  　 ||　 　 　 |
+ 　   　  ||￣￣￣￣￣
+　　   　 ||  君が代は
+   ∧__,,∧||　 千代に八千代に
+　 ( ｀･ω･||    さざれ石の巌となりて
+　  ヽ  つ０     こけのむすまで
+　　 し―-J
+---------------------------------------------------------------------------------------------------*/
+
+#else
+#include <bits/stdc++.h>
+using namespace std;
+#include <atcoder/all>
+using namespace atcoder;
+using ll = long long;
+using ull = unsigned long long;
+using ldb = long double;
+using P = pair<ll, ll>;
+#define fi first
+#define se second
+#define REP(i,a,b) for (ll i = a, i##_range = (b); i < i##_range; i++)
+#define rep(i,a) REP(i,0,a)
+template <class T> bool chmin(T& a, const T& b) { return a > b ? a = b, true : false; }
+template <class T> bool chmax(T& a, const T& b) { return a < b ? a = b, true : false; }
+#ifdef _DEBUG
+#include <debug.hpp>
+#define debug(...) debug::dprint(#__VA_ARGS__, __VA_ARGS__)
+#else
+#define debug(...) (static_cast<void>(0))
+#endif
+struct initialise { initialise() { cin.tie(nullptr); ios::sync_with_stdio(false); cout << fixed << setprecision(20); }; }__INI__;
+const ll inf = 1LL << 60;
+const ll dx[4] = {-1, 1, 0, 0}, dy[4] = {0, 0, -1, 1};
+#endif
