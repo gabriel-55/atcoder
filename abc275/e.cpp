47b743c3b40dce7ad75238bf93916f67f60036d0
Gabriel <renokoku@gmail.com>
2022-11-12 21:11:01 +1300

    commit

---
+++ abc275/e.cpp
@@ -0,0 +1,66 @@
+#if !__INCLUDE_LEVEL__
+#include __FILE__
+
+using mint = modint998244353;
+
+int main(void){
+   ll n, m, k;
+   cin >> n >> m >> k;
+   vector<mint> dp(n+1, 0);
+   dp[0] = 1;
+
+   mint ans = 0;
+   mint nm = mint(1)/m;
+   while(k--) {
+      vector<mint> p(n+1, 0);
+      swap(dp, p);
+      rep(i,n) REP(j,1,m+1) {
+         ll x = i+j;
+         if(x > n) x = n - (x-n);
+         dp[x] += p[i]*nm;
+      }
+      ans += dp[n];
+   }
+
+   cout << ans.val() << '\n';
+   return 0;
+}
+
+/*---------------------------------------------------------------------------------------------------
+　　 　　 ○＿＿＿＿＿_
+　  　 　 ||　 　 　 |
+　 　  　 ||　  ● 　 |
+　 　  　 ||　 　 　 |
+ 　   　  ||￣￣￣￣￣
+　　   　 ||  君が代は
+   ∧__,,∧||　 千代に八千代に
+　 ( ｀･ω･||    さざれ石の巌となりて
+　  ヽ  つ０     こけのむすまで
+　　 し―-J
+---------------------------------------------------------------------------------------------------*/
+
+#else
+#include <bits/stdc++.h>
+using namespace std;
+#include <atcoder/all>
+using namespace atcoder;
+using ll = long long;
+using ull = unsigned long long;
+using ldb = long double;
+using P = pair<ll, ll>;
+#define fi first
+#define se second
+#define REP(i,a,b) for (ll i = a, i##_range = (b); i < i##_range; i++)
+#define rep(i,a) REP(i,0,a)
+template <class T> bool chmin(T& a, const T& b) { return a > b ? a = b, true : false; }
+template <class T> bool chmax(T& a, const T& b) { return a < b ? a = b, true : false; }
+#ifdef _DEBUG
+#include <debug.hpp>
+#define debug(...) debug::dprint(#__VA_ARGS__, __VA_ARGS__)
+#else
+#define debug(...) (static_cast<void>(0))
+#endif
+struct initialise { initialise() { cin.tie(nullptr); ios::sync_with_stdio(false); cout << fixed << setprecision(20); }; }__INI__;
+const ll inf = 1LL << 60;
+const ll dx[4] = {-1, 1, 0, 0}, dy[4] = {0, 0, -1, 1};
+#endif
